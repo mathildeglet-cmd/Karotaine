4c3b9bfd25c6d7c97fce20a49cd44e94de6f8fd2
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-12-03 16:20:09 +0100

    feat: add loader recipePage

---
+++ src/pages/recette/RecettePage.tsx
@@ -0,0 +1,18 @@
+import style from "./recettePage.module.css";
+import { useLoaderData } from "react-router-dom";
+
+function About() {
+  const dataRecipe = useLoaderData();
+  console.log(dataRecipe);
+  return (
+    <>
+      {dataRecipe.meals.map((m) => (
+        <h1 key={m.idMeal} className={style.h1}>
+          {m.strMeal}
+        </h1>
+      ))}
+    </>
+  );
+}
+
+export default About;

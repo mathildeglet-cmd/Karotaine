d92fc09d5bbe98f446fc95a5ec1c7d5ac79f463d
Mathilde Glet <you@example.com>
2024-12-03 16:25:30 +0100

    feat: creation  definition type and mealcard

---
+++ src/components/MealCard.tsx
@@ -0,0 +1,10 @@
+export function MealCard() {
+  return (
+    <section>
+      <span>name</span>
+      <img src="strThumb" alt="a meal" />
+      <span>category</span>
+      <span> 3 ingrédients</span>
+    </section>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,18 @@
 import style from "./Home.module.css";
+import { MealCard } from "../../components/MealCard";
 
 function Home() {
   return (
+    <>
     <div>
       <h1 className={style.h1title}>Home</h1>
       <button className="favorite styled" type="button">
         click here
       </button>
     </div>
+
+    <MealCard/>
+    </>
   );
 }
 

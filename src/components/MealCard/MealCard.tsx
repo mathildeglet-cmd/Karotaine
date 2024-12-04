9734862e7109ec317af5ad625b87a17cb7cd07e9
Mathilde Glet <you@example.com>
2024-12-04 10:29:27 +0100

    feat: mealcard logic and style finished

--- src/components/MealCard/MealCard.tsx
+++ src/components/MealCard/MealCard.tsx
@@ -1,20 +1,49 @@
 import type { MealDataType } from "../../lib/definitions";
 import style from "./MealCard.module.css";
+import { useState } from "react";
+
+type isOpenDetailsType = {
+  isOpenDetails: boolean;
+  setIsOpenDetails: (b: boolean) => void;
+};
 
 export function MealCard({ data }: { data: MealDataType }) {
+  const [isOpenDetails, setIsOpenDetails] = useState<
+    boolean | (() => isOpenDetailsType)
+  >(false);
+
+  const handleIsOpenDetails = () => {
+    setIsOpenDetails(!isOpenDetails);
+  };
+
   return (
-    <section className={style.cards}>
-      <h2 className={style.mealName}>{data.strMeal}</h2>
-      <img className={style.picture} src={data.strMealThumb} alt="a meal" />
-      <h2 className={style.h2ingredient}>Ingredients : </h2>
-      <ul className={style.listIngredient}>
-        <li className={style.ingredient}>{data.strIngredient1}</li>
-        <li className={style.ingredient}>{data.strIngredient2}</li>
-        <li className={style.ingredient}>{data.strIngredient3}</li>
-        <li className={style.ingredient}>{data.strIngredient4}</li>
-        <li className={style.ingredient}>{data.strIngredient5}</li>
-        <li className={style.ingredient}>{data.strIngredient6}</li>
-      </ul>
-    </section>
+    <button type="button" onClick={handleIsOpenDetails}>
+      <section className={style.cards}>
+        {!isOpenDetails && (
+          <section>
+            <h2 className={style.mealName}>{data.strMeal}</h2>
+            <img
+              className={style.picture}
+              src={data.strMealThumb} 
+              alt="a meal"
+            />
+          </section>
+        )}
+        {isOpenDetails && (
+          <section className={style.versoCard}>
+            <h2 className={style.h2ingredient}>Ingredients : </h2>
+            <ul className={style.listIngredient}>
+              <li className={style.ingredient}>{data.strIngredient1}</li>
+              <li className={style.ingredient}>{data.strIngredient2}</li>
+              <li className={style.ingredient}>{data.strIngredient3}</li>
+              <li className={style.ingredient}>{data.strIngredient4}</li>
+              <li className={style.ingredient}>{data.strIngredient5}</li>
+              <li className={style.ingredient}>{data.strIngredient6}</li>
+            </ul>
+          </section>
+        )}
+      </section>
+      <button type="button" className={style.eatButton}>MANGER</button>
+    </button>
   );
 }
